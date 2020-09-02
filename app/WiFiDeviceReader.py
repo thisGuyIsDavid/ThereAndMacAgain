6a2e4f1e34a271d980996f854e125a6aff1fdd45
David <dhaverberg@medicalalley.org>
2020-09-01 20:35:13 -0500

    new gps

--- app/WiFiDeviceReader.py
+++ app/WiFiDeviceReader.py
@@ -118,8 +118,6 @@ class WiFiDeviceReader:
 	def process_collected_data(self):
 		collected_data = {**self.gps_data, **self.wifi_data}
 		cleaned_data = {key: value if value != '' else None for key, value in collected_data.items()}
-		print(cleaned_data)
-
 		key_name = "%s_%s_%s" % (cleaned_data.get('mac_address'), cleaned_data.get('latitude'), cleaned_data.get('longitude'))
 		# check cache
 		if self.redis_cache.is_key_in_store(key_name):
