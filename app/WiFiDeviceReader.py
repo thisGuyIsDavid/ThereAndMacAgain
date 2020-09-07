7d1b280a0b7258bf2a1565f0ec7f9b7a7abb1611
David <dhaverberg@medicalalley.org>
2020-09-06 21:10:22 -0500

    new gps

--- app/WiFiDeviceReader.py
+++ app/WiFiDeviceReader.py
@@ -68,7 +68,7 @@ class WiFiDeviceReader:
 	@staticmethod
 	def process_gps_data(gps_data):
 		gps_array = [x for x in gps_data.strip().split('|') if x != '']
-		print(gps_data)
+		print(gps_array)
 		latitude = round(float(gps_array[0]), 4)
 		longitude = round(float(gps_array[1]), 4)
 		return {
