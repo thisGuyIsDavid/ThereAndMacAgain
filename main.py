5397d2239941b4d5ddbe7e9dc43c2b03ac61c965
David <dhaverberg@medicalalley.org>
2020-10-02 18:19:09 -0500

    new gps

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from app.collector.MainCollector import MainCollector
 
 if __name__ == '__main__':
-    MainCollector(
-        wifi_port='/dev/ttyUSB1',
-        gps_port='/dev/ttyUSB0',
-        database_location='/home/pi/tama.db'
-    ).run()
+    MainCollector(wifi_port='/dev/ttyUSB1', gps_port='/dev/ttyUSB0').run()
