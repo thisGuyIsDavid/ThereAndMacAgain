a53676cc0dbf4228ce63d0aa17a01f2108fa3b48
David <dhaverberg@medicalalley.org>
2020-10-02 18:04:44 -0500

    new gps

---
+++ app/collector/MainProcessor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import pika, sys, os, json
+
+
+class MainProcessor:
+
+    def __init__(self):
+        pass
+
+
+    def process(self, to_process):
+        print(to_process)
+
+    pass
--- sandbox.py
+++ sandbox.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 import pika, sys, os, json
-
+from app.collector.MainProcessor import MainProcessor
 
 
 def main():
+    #   set processor
+    main_processor = MainProcessor()
+
+    #   set consumer
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
-
     channel.queue_declare(queue='there_and_mac_again')
 
+    #   set callback
     def callback(ch, method, properties, body):
         received_data = json.loads(body)
+        main_processor.process(received_data)
 
-        print(" [x] Received %r" % received_data)
-
+    #   start listening
     channel.basic_consume(queue='there_and_mac_again', on_message_callback=callback, auto_ack=True)
     channel.start_consuming()
 
