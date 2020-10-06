e1e11165da44e2f4832498f2b20f93bb835b50e3
David <dhaverberg@medicalalley.org>
2020-10-06 13:53:43 -0500

    new gps

--- sandbox.py
+++ sandbox.py
@@ -30,7 +30,10 @@ def main():
         events = get_key()
         if events:
             for event in events:
-                print(event.code, keypad_inputs.get(event.code, 'hello'))
+                key_pressed = keypad_inputs.get(event.code, None)
+                if key_pressed is None:
+                    continue
+                print(event.code)
 
 if __name__ == "__main__":
     main()
