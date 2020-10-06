bed315084a71402388770bc332afa18dd7bc5216
David <dhaverberg@medicalalley.org>
2020-10-06 13:39:31 -0500

    new gps

--- app/utils/inputs.py
+++ app/utils/inputs.py
@@ -909,10 +909,8 @@ class BaseListener(object):  # pylint: disable=useless-object-inheritance
             self.update_timeval()
             timeval = self.timeval
         try:
-            if event_type == 'LED':
-                return
-            print(event_type)
             event_code = self.type_codes[event_type]
+            print(event_code)
         except KeyError:
             raise UnknownEventType(
                 "We don't know what kind of event a %s is." % event_type)
